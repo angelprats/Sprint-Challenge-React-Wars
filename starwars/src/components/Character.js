5c87400134d0119d7dd25296813906f7824a439d
angel prats <angelpratz@gmail.com>
2019-05-10 13:26:41 -0400

    started css

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,16 +1,18 @@
 import React from 'react';
+import './StarWars.css';
+
 
 const Character = props => {
   return (
-    <div>
-      <div>
+    <div className='card'>
+      <div className='name'>
         <h1>
           {props.charProp.name}
         </h1>
       </div>
 
 
-      <div>
+      <div className='info'>
         <h2>BirthDate: {props.charProp.birth_year}</h2>
         <h2>Hair Color: {props.charProp.color}</h2>
         <h2>Height: {props.charProp.height}</h2>
