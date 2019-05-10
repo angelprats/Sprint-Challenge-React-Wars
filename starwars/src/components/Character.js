a9577a1fd237332047d227031a6a01f5ef62084f
angel prats <angelpratz@gmail.com>
2019-05-10 12:52:00 -0400

    completed character component have failed to compile bug

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -4,12 +4,19 @@ const Character = props => {
   return (
     <div>
       <div>
-
+        <h1>
+          {props.charProp.name}
+        </h1>
       </div>
+
 
       <div>
-
+        <h2>BirthDate: {props.charProp.birth_year}</h2>
+        <h2>Hair Color: {props.charProp.color}</h2>
+        <h2>Height: {props.charProp.height}</h2>
+        <h2>Gender: {props.charProp.gender}</h2>
       </div>
+
     </div>
   );
 }
